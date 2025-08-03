860f375cf3f7342026cd0ab3e6b0eba6612b911f
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2025-08-03 17:44:29 +0300

    change border

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// const iconMenu = document.querySelector(".icon-menu");
-// iconMenu.addEventListener("click", () => {
-//   iconMenu.classList.toggle("menu-open");
-//   document.documentElement.classList.toggle("menu-open");
-// });
-
 document.addEventListener("click", documentActions);
 
 function documentActions(e) {
